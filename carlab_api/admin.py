5cc171761e3c3f67199877498eadba9e20a93a26
rRios15041995 <rodolfo.r1504@gmail.com>
2018-01-22 16:05:18 -0600

    s

--- carlab_api/admin.py
+++ carlab_api/admin.py
@@ -2,13 +2,13 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Service, Category, SubCategory, User, Worker, Order
-
-admin.site.register(Service)
-admin.site.register(Category)
-admin.site.register(SubCategory)
-admin.site.register(User)
-admin.site.register(Worker)
-admin.site.register(Order)
+# from .models import Service, Category, SubCategory, User, Worker, Order
+#
+# admin.site.register(Service)
+# admin.site.register(Category)
+# admin.site.register(SubCategory)
+# admin.site.register(User)
+# admin.site.register(Worker)
+# admin.site.register(Order)
 
 # Register your models here.
